db6db233b74fb68277ac7d771f7368ebc393b4b6
dharmendra3840 <dharmendrak3327mahato@gmail.com>
2025-10-15 03:38:11 +0530

    Add dynamic avatar, editable DP, and recent posts display with dynamic counts

--- Models/user.model.js
+++ Models/user.model.js
@@ -3,6 +3,16 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   uuidPassword: { type: String, required: true },
-});
+  profilePicture: { 
+    filename: String,
+    originalName: String,
+    mimetype: String,
+    size: Number,
+    path: String
+  },
+  followers: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+  following: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+  bio: { type: String, default: "" },
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
